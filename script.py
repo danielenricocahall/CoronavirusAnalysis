d35c2ad6dab0773477b8158819a415b100734e85
danielenricocahall <danielenricocahall@gmail.com>
2020-03-22 22:27:35 -0400

    simplify script

--- script.py
+++ script.py
@@ -17,16 +17,9 @@ df = pd.read_csv(response)
 italian_data = df[df['Country/Region'] == 'Italy']
 us_data = df[df['Country/Region'] == 'US']
 nj_data = us_data[us_data['Province/State'] == 'New Jersey']
-x = []
-y = []
 
-counter = 1
-for column in nj_data.columns:
-    if '/20' in column:
-        x.append(counter)
-        y.append(nj_data[column].iloc[0].item())
-        counter += 1
-
+y = [nj_data[column].iloc[0].item() for column in nj_data.columns if '/20' in column]
+x = [i for i, _ in enumerate(y)]
 popt, pcov = curve_fit(func, np.array(x), y)
 plt.plot(x, y, label='true')
 a, b, c = tuple(popt)
