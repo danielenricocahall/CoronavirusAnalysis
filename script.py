04e13c8c8e9fa03a46d124d5a62a9647c81096b0
danielenricocahall <danielenricocahall@gmail.com>
2020-03-22 22:19:50 -0400

    formatting

--- script.py
+++ script.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 from urllib.request import urlopen
+
 url = 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Confirmed.csv&filename=time_series_2019-ncov-Confirmed.csv'
 
+
 def func(x, a, b, c):
-    return a * b**x + c
+    return a * b ** x + c
 
 
 response = urlopen(url)
@@ -24,7 +26,6 @@ for column in nj_data.columns:
         x.append(counter)
         y.append(nj_data[column].iloc[0].item())
         counter += 1
-
 
 popt, pcov = curve_fit(func, np.array(x), y)
 plt.plot(x, y, label='true')
